f0cfd3a22ed3db8f96ef9b1049a86ef9874602c2
manishewag <160532465+manishewag@users.noreply.github.com>
2024-10-24 15:51:36 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URi);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`connected to ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
